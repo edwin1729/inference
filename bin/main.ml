14ca1d148ebd8b868e011e136894541809f14629
Edwin Fernando <ef322@ic.ac.uk>
2025-06-24 20:05:24 +0100

    completed untested

--- bin/main.ml
+++ bin/main.ml
@@ -17,16 +17,34 @@ type e =
   | Val of int
 
 type lType = (*language type*)
+  | T (*Top, the type of a raise expression*)
   | LInt              (* Language Int *)
   | TyVar of int
-  | TyAbs of int * lType
-  | EffAbs of int * lType
   | Arr of lType * row * lType (* arrow type *)
-  | T (*Top, the type of a raise expression*)
+(*  | TyAbs of int * lType*)
+(*  | EffAbs of int * lType*)
 and sigma =
   | Sig of exc
   | EffVar of int (*aka row variable*)
 and row = sigma list ref (*This is mutable to make the unify function look cleaner*)
+
+let compareSig (s1: sigma) (s2: sigma): int = match (s1, s2) with
+    (Sig (Exc n1), Sig (Exc n2)) -> Int.compare n1 n2
+  | ((Sig exc), (EffVar n)) -> -1
+  | ((EffVar n), (Sig exc)) -> 1
+  | ((EffVar n1), (EffVar n2)) -> Int.compare n1 n2
+
+(* Used as set of effect names*)
+module RowSet = Set.Make(struct
+  type t = sigma
+  let compare = compareSig
+end)
+
+let rec mapLType (f: int -> lType) (t: lType): lType = match t with
+   LInt -> LInt
+  | (TyVar (n)) -> f n
+  | (Arr (t1, r, t2)) -> Arr (mapLType f t1, r, mapLType f t2) (*rows don't have type variables*)
+  | T -> T
 
 (* Used as map from int to lType *)
 module IntMap = Map.Make(struct
@@ -43,8 +61,8 @@ end)
 open IntMap
 (*type context = lType IntMap.t Context where the int denotes a term variable *)
 
-type context = {ty: lType IntMap.t; eff: IntSet.t} (* Context where the int denotes a term variable *)
-type subs = Subs of lType IntMap.t  (* Substitution where int denotes a type and row variables *)
+type context = {ty: lType IntMap.t; eff: IntSet.t} (* Context where for `ty` the int denotes a term variable *)
+(*outdated*)type subs = Subs of lType IntMap.t  (* Substitution where int denotes a type and row variables *)
 
 let lookup ctx var =
   IntMap.find_opt var ctx
@@ -64,14 +82,34 @@ let freshT: unit -> int = (*fresh type variables*)
     v*)
 
 (*let subsContext (subs: subs) (context: context): context = failwith ""*)
-let subsContext (subs: lType -> lType) (context: context): context = failwith ""
-let subsLType (subs: subs) (t: lType): lType = failwith ""
-let globalEffects (exc: exc): lType = failwith "" (*assuming no type variables here*)
+let subsContext (subs: lType -> lType) (ctx: context): context = IntMap.map subs ctx.ty
+let globalEffects ((Exc n): exc): lType = match n with (*assuming no type variables here*)
+  1 -> LInt
+  | 2 -> Arr (LInt, ref [], LInt)
+
+let rowSubsume (r1: row) (r2: row) =
+  let rs1 = List.fold_left (fun acc x -> RowSet.add x acc) RowSet.empty !r1 in
+  let rs2 = List.fold_left (fun acc x -> RowSet.add x acc) RowSet.empty !r2 in
+  let union = RowSet.elements (RowSet.union rs1 rs2) in
+  r1 := union;
+  r2 := union
 
 (*Performs the subsumption rule as an when required. It feels natural for unify to universaldeal with subsumption
   But this is just a design choice. Another choice I've made is that if subsumption the new type needs to be
   returned. I've decided to try doing this by making row variables mutable references always*)
-let unify (t1: lType) (t2: lType): (lType -> lType) option = failwith ""
+let rec unify (t1: lType) (t2: lType): (lType -> lType) option = match (t1, t2) with
+  (_, T) -> Some (Fun.id)
+  |(T, _) -> Some (Fun.id)
+  |(LInt, LInt) -> Some (Fun.id)
+  |(LInt, _) -> None (* where does the opposite case go? *)
+  |(TyVar n, ty) -> Some (mapLType (fun n -> ty))
+  |(Arr (t1, r, t2), LInt) -> None
+  |(Arr (t1, r, t2) as arr, (TyVar n as tyvar)) -> unify tyvar arr
+  |(Arr (t1, r, t2), Arr (t1', r', t2')) ->
+    let* s1 = unify t1 t1' in
+    let* s2 = unify (s1 t2) (s1 t2') in
+    let _ = rowSubsume r r' in
+    Some (s2 % s1)
 
 (* Applies the typing rules enhanced by an implicit subsumption rule which is
    always applied through a call to unify *)
@@ -96,20 +134,25 @@ let rec infer (ctx: context) (e : e) : ((lType -> lType) * lType * row) option =
   | Var tyvar ->
       let* t = find_opt tyvar ctx.ty in
       Some (Fun.id, t, ref [])
-  | Try (e1, (Exc n as exc), f_h, f_r) ->
+  | Try (e1, (Exc n as exc), h, r) ->
       let* (s1, t1, r1) = infer ({ctx with eff = IntSet.add n ctx.eff}) e1 in
       let effTy = globalEffects exc in
       let c1 = subsContext s1 ctx in
-      let* (s_h, t_h, r_h) =  infer c1 f_h in (* Shallow handler, should it be deep? *)
+      let* (s_h, t_h, r_h) =  infer c1 h in
       let r1' = ref (List.filter (fun x -> x <> (Sig exc)) !r1) in
       let phi = TyVar (freshT ()) in
-      let* s_h' = unify t_h (Arr (effTy, r1', phi)) in
+      let* s_h' = unify t_h (Arr (effTy, r1', phi)) in (*fresh ref?*)
       let c_h = subsContext (s_h' % s_h) c1 in
-      let* (s_r, t_r, r_r) = infer c_h f_r in
-      (*let* s_r' = unify t_r Arr (t1, r1', )*)
-      failwith ""
-  | Raise (exc, e) ->   failwith ""
-  | Val _ ->          Some (Fun.id, LInt, ref [])
+      let* (s_r, t_r, r_r) = infer c_h r in
+      let* s_r' = unify t_r (Arr (t1, r1', s_r phi)) in (*fresh ref?*)
+      Some (s_r' % s_r % s_h' % s_h, (s_r' % s_r) phi, r1')
+  | Raise (Exc n as exc, e) ->
+      let* _ = IntSet.find_opt n ctx.eff in
+      let* (s, t, r) = infer ctx e in
+      let effTy = globalEffects exc in (*no type var allowed*)
+      let _s = unify t effTy in (*must be id subs, so ignore*)
+      Some (s, T, ref [])
+  | Val _ -> Some (Fun.id, LInt, ref [])
 let () =
   let () = print_int (freshT ()) in
   print_int (freshT ())
